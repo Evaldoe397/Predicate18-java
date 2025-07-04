152e4841b9033d83e3b9e39b36fda887b02a31ae
evaldoe397 <evaldoe397@gmail.com>
2025-07-04 00:35:05 -0300

    removendo de uma lista com a interface predicate

---
+++ src/Application/Program.java
@@ -0,0 +1,31 @@
+package Application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import Entities.Product;
+import Util.ProductPredicate;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		//Utilizando uma classe como interface com a implementação
+		//"Predicate" e instanciando dentro do removeIf
+		list.removeIf(new ProductPredicate());
+		
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
+
+	
---
+++ src/Util/ProductPredicate.java
@@ -0,0 +1,12 @@
+package Util;
+
+import java.util.function.Predicate;
+import Entities.Product;
+
+public class ProductPredicate implements Predicate<Product>{
+
+	@Override
+	public boolean test(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+}
